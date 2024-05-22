79ae6f2ebd642557b5ad62a748433b8d66c64e86
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-05-22 23:39:37 +0530

    Add StopWatch Clean Up function

---
+++ src/components/StopWatch.jsx
@@ -0,0 +1,41 @@
+// src/components/Stopwatch.jsx
+
+import { useState, useEffect } from "react";
+
+function StopWatch() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+   
+    /*Complete the missing code*/
+    const IntervalId = setInterval(() => {
+
+  
+      setCount((prevCount) => {
+        if (prevCount < 10) {
+          return prevCount + 1;
+        } else {
+          clearInterval(IntervalId);
+          return prevCount;
+        }
+      });
+
+    } ,1000)
+
+
+    function cleanFunction(){
+      clearInterval(IntervalId)
+    }
+
+    return cleanFunction
+  }, []);
+
+  return (
+    <div style={{ border: "1px dashed red", padding: "10px", margin: "10px" }}>
+      <h1>Stop Watch</h1>
+      <h4>{count}</h4>
+    </div>
+  );
+}
+
+export default StopWatch;
